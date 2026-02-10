4481528f3d8d7a680a149ba1c8a3f966fe342bd2
Sraeshta <sraeshtasabarish@outlook.com>
2026-02-10 15:23:41 +0530

    created an application that can auto websearch

--- ws/ws.c
+++ ws/ws.c
@@ -1,3 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-
+int main(int argc, char *argv[]){
+	int numArgs = argc -1;
+	char* search = argv[1];
+	char url[]= "xdg-open https://duckduckgo.com/?q=";
+	if( numArgs > 1)
+	{
+		printf("too many arguments....\n");
+		return 0;
+	}
+	if(numArgs == 0)
+	{
+		system("xdg-open https://duckduckgo.com/?q=");
+		return 0;
+	}
+	strcat(url, search);
+	system(url);
+	return 0;
+}
